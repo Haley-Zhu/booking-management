48bc5109c550483d8e45af2d540f978e35a99f3e
Haley-Zhu <Haley.ZhuY@gmail.com>
2020-06-13 23:44:24 +1000

    add GlobalLayout

---
+++ src/components/GlobalLayout.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Layout, Menu } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import logo from '../assets/icons/logo-text.png';
+
+const { Header, Sider, Content } = Layout;
+
+class GlobalLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <Layout id="global-layout">
+        <Sider className="global-layout__sider" trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="global-layout-logo" >
+            <img src={logo} alt="logo" />
+          </div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="global-layout__content">
+          <Header className="global-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: this.toggle,
+              }
+            )}
+          </Header>
+          <Content
+            className="global-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            {this.props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default GlobalLayout;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -3,11 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/User/Login';
 import Signup from '../pages/User/Signup';
 import Order from '../pages/Order/Order';
+import GlobalLayout from '../components/GlobalLayout';
 
 const Routes = () => {
   return (
     <Switch>
+      <GlobalLayout>
       <Route exact path='/login' component={Login} />
+      </GlobalLayout>
+      {/* <Route exact path='/login' component={Login} /> */}
       <Route exact path='/signup' component={Signup} />
       <Route path='/orders' component={Order} />
     </Switch>
